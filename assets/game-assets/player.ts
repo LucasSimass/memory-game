f234554161ec7e7aa2506ab1586e1e32cb92d68e
Lucas Simas <simaslucas1m@gmail.com>
2026-02-24 04:55:12 -0300

    Adding player pick up card feature

--- assets/game-assets/player.ts
+++ assets/game-assets/player.ts
@@ -1,8 +1,76 @@
 export class Player { 
-  name: string = "";
-  cardsChoosed: number[] = []; // every sequal of chose user did (and not failed)
+  private name: string = "";
+  private cardsChoosed: number[] = []; // every sequal of chose user did (and not failed)
 
   constructor({name}: {name: string}) {
     this.name = name;
+  }
+
+  public get getName() : string {
+    return this.name;
+  }
+
+  public get getCardsChoosed() : number[] {
+    return this.cardsChoosed;
+  }
+
+  /**
+   * action of user to pick up an card and check if it's valid
+   * 
+   * @param {number} index - the index of the card choosed
+   * 
+   * @param {number[]} cards - the cards on the game
+   * 
+   * @returns true, if the last card number choosed is the same as choosed or it was the first one or if the last card number and penultimate card number is the same
+   * 
+   * @returns false, if the last card doesn't match the last card choosed 
+   * 
+  */
+  public chooseCard(index: number, cards: number[]) : boolean {
+    if (index < 0 || index > cards.length - 1 ) {
+      throw new Error("index should be > 0 and <=cards.length")
+    }
+
+    // num of card choosed
+    const numChoosed = cards[index]!;
+
+    // TODO:
+    // 1) check if the last index is the same as the currently
+
+    // if cardsChoosed lenght == 0, can go on! 
+    if (this.cardsChoosed.length == 0) {
+      this.cardsChoosed.push(cards[index]!);
+      return true;
+    }
+
+    // if card choosed is the same as the last on cardsChoosed
+    if (this.cardsChoosed[this.cardsChoosed.length - 1] == numChoosed){
+      this.cardsChoosed.push(cards[index]!);
+      return true;
+    }
+
+    // if the last cards was the same it means that player can choose another
+    if (this.cardsChoosed[this.cardsChoosed.length - 2] == this.cardsChoosed[this.cardsChoosed.length - 1]){
+      this.cardsChoosed.push(cards[index]!);
+      return true;
+    }
+
+
+    // clear if is user fail
+    // and returns false
+    this.cardsChoosed = [];
+    return false;
+  }
+
+
+  /**
+   * Check if user win.
+   * 
+   * @param {number[]} cards - the game's cards
+   * 
+   * @returns boolean, check if the user have the same length of the cards
+  */
+  public isPlayerWin(cards: number[]) {
+    return this.cardsChoosed.length == cards.length 
   }
 }
--- memory-game.ts
+++ memory-game.ts
@@ -3,21 +3,65 @@ import { Player } from "./assets/game-assets/player.ts";
 import type { startGameType } from "./assets/types/types.ts";
 
 export class MemoryGame {
-  p1: Player | null = null;
-  p2: Player | null = null;
+  private p1: Player | null = null;
+  private p2: Player | null = null;
 
-  cardsGame: number[] | null = null
+  private cards: number[] = []
+
+  private isP1Turn: boolean = true;
+  private isGameDone: boolean = false;
+  private whoWin: string = ""; 
 
   constructor({p1Name, p2Name, maxNumCards}: startGameType) {
     this.p1 = new Player({name: p1Name});
     this.p2 = new Player({name: p2Name});
     
-    this.cardsGame = CardsGenerator.generateCards(maxNumCards);
+    this.cards = CardsGenerator.generateCards(maxNumCards);
+  }
+  
+  // which index user will choose to pick an card?
+  public pickUpCard(index: number) {
+    const isPlayerSucessChoose = this.isP1Turn 
+    ? this.p1!.chooseCard(index, this.cards) 
+    : this.p2!.chooseCard(index, this.cards);
+
+    if (this.p1!.isPlayerWin(this.cards)) {
+      this.isGameDone = true;
+      this.whoWin = this.p1!.getName;
+      return;
+    }
+
+    if (this.p2!.isPlayerWin(this.cards)) {
+      this.isGameDone = true;
+      this.whoWin = this.p2!.getName;
+      return;
+    }
+
+    if (!isPlayerSucessChoose){
+      this.isP1Turn = !this.isP1Turn;
+    }
+
+    return;
   }
 
-  
-  public get cardGame() : number[] {
-    return this.cardGame
+  public get getCards() : number[] {
+    return this.cards!
   }
-  
+
+  // returns the cards blured
+  public get getBlurCards(): string[] {
+    return this.cards.map(card => "*");
+  }
+
+  public get getIsP1Turn() : boolean {
+    return this.isP1Turn
+  }
+
+  public get getIsGameDone() {
+    return this.isGameDone;
+  }
 }
+
+const mGame = new MemoryGame({p1Name: "Leo", p2Name: "Eduard", maxNumCards: 10});
+console.log(mGame.getBlurCards);
+
